416d6090a32c900ddaede6ea917ae1d18bf86e92
andresvr2204 <andresromerovr@gmail.com>
2025-11-10 11:51:57 -0500

    evaluacion de simulacion de factura
    .

---
+++ EVALUACION - ANDRES VILLALBA/evaluacion2.js
@@ -0,0 +1,22 @@
+//elaborar un algoritmo que muestre el total pagado de una compra con x cantidad de productos. aparte del total debe mostrar el iva con descuento del 5 % si la compra es del 1000% la persona paga en efectivo por ende debe mostrar cuanto es las vueltas//
+
+let total ;
+let cantidad;
+let num1 = 1;
+let num2 = 2;
+let 
+function compra (){
+    total = num1 + num2 
+
+    switch (total) {
+        case v:
+            
+            break;
+    
+        default:
+            break;
+    }
+    
+    
+
+}

223c1c590dce1d160751cf4bfa1d80dd7c9fddec
sam1988-miami <smanrique192@gmail.com>
2025-11-10 11:36:19 -0500

    parte 4 Samuel Manrique

---
+++ parte3.js
@@ -0,0 +1,5 @@
+//algoritmo que muestre cual que es promedio de las edades de los aprendices de la ficha y cuantos son mayores de edad: 
+let edades;
+for (i = 0 > 22;);
+i++;
+let pregunta = prompt("cual es tu edad?");

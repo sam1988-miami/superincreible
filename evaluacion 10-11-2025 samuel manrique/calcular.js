0088fa2c6684ce0af7e19c9c301776dd8921ae3f
sam1988-miami <smanrique192@gmail.com>
2025-11-10 11:06:32 -0500

    calcular triangulo samuel manrique

---
+++ evaluacion 10-11-2025 samuel manrique/calcular.js
@@ -0,0 +1,5 @@
+let calcular_triangulo;
+let base = parseFloat(prompt("digite la base"));
+let altura = parseFloat(prompt("digite la altura"));
+let calcular = (base * altura);
+console.log(calcular);

5b950b9c5811ce13bf127f189c24ffd1ad4840d0
sam1988-miami <smanrique192@gmail.com>
2025-11-10 11:24:00 -0500

    Samuel Manrique parte 2

---
+++ parte2.js
@@ -0,0 +1,5 @@
+let multiplicacion;
+let num1 = parseInt(prompt("digite el primer numero: "));
+let num2 = parseInt(prompt("digite el segundo numero: "));
+let operacion = num1 * num2;
+console.log ("su resultado es", {operacion});

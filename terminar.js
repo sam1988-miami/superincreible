73f3f8fae2cd2ea5b2a47b7e9ffd42a28d25c5ee
ManuelVasquez-hipe <drules369@gmail.com>
2025-11-10 11:35:14 -0500

    dfgvhb
    yyfcgvhbn

---
+++ terminar.js
@@ -0,0 +1,8 @@
+/*elabore un algoritmo que muestre el promedio de las edades 
+de la ficha y cuantos son mayores de edad*/
+int i = 0;
+X = 52
+while (X >= i){
+    System.out.dialog( "digite las ")
+    i= i ++
+}

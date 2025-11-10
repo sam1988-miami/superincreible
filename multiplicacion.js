c7377a400fd7e8d35edec47e88685c973c92eac8
Johan Esteban Acosta <ea054196@gmail.com>
2025-11-10 11:23:35 -0500

    Multiplicar 2 numeros

---
+++ multiplicacion.js
@@ -0,0 +1,8 @@
+let Num1 = Number(prompt("Digite el numero 1"));
+let Num2 = Number(prompt("Digite el numero 2"));
+let operacion;
+console.log(multi(Num1, Num2));
+function multi(Num1, Num2){
+    operacion = Num1 * Num2;
+    return operacion;
+}
